63a3055a66b5b0a1efa4f68e1ce0e0fb7686ab20
forestli2022 <forestli2022@gmail.com>
2025-08-03 17:10:09 +0100

    Fix: make sure no token is present in job scripts

--- job_prune.sh
+++ job_prune.sh
@@ -16,8 +16,6 @@ conda activate auto-merge-llm
 cd $PBS_O_WORKDIR
 
 # Set huggingface token and login
-export HF_TOKEN=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
-huggingface-cli login --token $HF_TOKEN
 
 # Optional: Debug check
 python --version
